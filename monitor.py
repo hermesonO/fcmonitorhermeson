13c88d889f3a8d937e72428b580e7b62d2a8a057
hermesonO <hermesonatila50@gmail.com>
2025-09-26 17:43:33 -0300

    Create monitor.py

---
+++ monitor.py
@@ -0,0 +1,85 @@
+{
+  "ok": true,
+  "result": [
+    {
+      "update_id": 534068167,
+      "message": {
+        "message_id": 2,
+        "from": {
+          "id": 430434779,
+          "is_bot": false,
+          "first_name": "Hermeson",
+          "username": "hermesono",
+          "language_code": "pt-br"
+        },
+        "chat": {
+          "id": 430434779,
+          "first_name": "Hermeson",
+          "username": "hermesono",
+          "type": "private"
+        },
+        "date": 1758917685,
+        "text": "/start",
+        "entities": [
+          {
+            "offset": 0,
+            "length": 6,
+            "type": "bot_command"
+          }
+        ]
+      }
+    },
+    {
+      "update_id": 534068168,
+      "message": {
+        "message_id": 3,
+        "from": {
+          "id": 430434779,
+          "is_bot": false,
+          "first_name": "Hermeson",
+          "username": "hermesono",
+          "language_code": "pt-br"
+        },
+        "chat": {
+          "id": 430434779,
+          "first_name": "Hermeson",
+          "username": "hermesono",
+          "type": "private"
+        },
+        "date": 1758917905,
+        "text": "https://api.telegram.org/botSEU_TOKEN/getUpdates",
+        "entities": [
+          {
+            "offset": 0,
+            "length": 48,
+            "type": "url"
+          }
+        ],
+        "link_preview_options": {
+          "is_disabled": true
+        }
+      }
+    },
+    {
+      "update_id": 534068169,
+      "message": {
+        "message_id": 4,
+        "from": {
+          "id": 430434779,
+          "is_bot": false,
+          "first_name": "Hermeson",
+          "username": "hermesono",
+          "language_code": "pt-br"
+        },
+        "chat": {
+          "id": 430434779,
+          "first_name": "Hermeson",
+          "username": "hermesono",
+          "type": "private"
+        },
+        "date": 1758917962,
+        "text": "TESTANDO BOT"
+      }
+    }
+  ]
+}
